184000e1bac3e6cd6ebe52e6713b656928d3c546
Devon Weller <devon@tokenly.co>
2016-10-05 19:23:10 -0500

    added example responses

--- examples/wallet-get-address.php
+++ examples/wallet-get-address.php
@@ -19,7 +19,7 @@ $wallet = CopayWallet::newWalletFromPlatformSeedAndWalletSeed($platform_seed, $w
 $copay_client = new CopayClient('https://pockets-service.tokenly.com/bws/api');
 
 try {
-    $result = $copay_client->getAddress($wallet['copayerId'], $wallet['requestPrivKey']);
+    $result = $copay_client->getAddressInfo($wallet['copayerId'], $wallet['requestPrivKey']);
 } catch (CopayException $e) {
     echo "Error ".$e->getCode().": ".$e->getMessage()." (".$e->getCopayStatusCode().")\n";
     exit(1);
@@ -27,3 +27,20 @@ try {
 
 echo "\$result: ".json_encode($result, 192)."\n";
 
+/*
+$result: {
+    "version": "1.0.0",
+    "createdOn": 1475616439,
+    "address": "39VbaiHFKTzwCvjFJczdjVQX11cVgu29g1",
+    "walletId": "72abff08-6414-4d91-adfc-509cbc26e53f",
+    "network": "livenet",
+    "isChange": false,
+    "path": "m/0/0",
+    "publicKeys": [
+        "03de0c5325e83ca7e66419edaf4b6b8af8a707858128f8be01b9d7c3bab48d3768",
+        "0240efc0a6f2583c47ba3407d2ad83fed26421dddad0b974a6cd7d8b97007cd701"
+    ],
+    "type": "P2SH",
+    "hasActivity": null
+}
+*/
--- examples/wallet-get.php
+++ examples/wallet-get.php
@@ -36,3 +36,59 @@ echo "\$result: ".json_encode($result, 192)."\n";
 // calculate the secret code
 $invitation_code = $wallet->getCopayerSecretInvitationCode($result['wallet']['id']);
 echo "Copayer secret invitation code:\n$invitation_code\n";
+
+/*
+{
+    "wallet": {
+        "version": "1.0.0",
+        "createdOn": 1475598272,
+        "id": "72abff08-6414-4d91-adfc-509cbc26e53f",
+        "name": "{\"iv\":\"rd4soA+NYsc3ah+4mj/HKw==\",\"v\":1,\"iter\":1,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"ct\":\"SbhHwk3+IBDouT8lHkXnTf+kUyd5rw==\"}",
+        "m": 2,
+        "n": 2,
+        "singleAddress": true,
+        "status": "complete",
+        "copayers": [
+            {
+                "version": 2,
+                "createdOn": 1475605970,
+                "id": "5b8d82daf6b45d10cc83619accb8cf9ce04101513ba2292b14886c7055f1ce26",
+                "name": "{\"iv\":\"Q4xx34p5cEXoVRc5tqYPBQ==\",\"v\":1,\"iter\":1,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"ct\":\"uOJkVKreP2fnIk+W4M7/IeUS2DmCaNg=\"}",
+                "requestPubKeys": [
+                    {
+                        "key": "02b01a396508903e4bd269ebc49cc770c4a4b775fd6d36c5f2c600799def55f46d",
+                        "signature": "3045022100912bea2d9ba884c79d052fbdfe4f303fa9cd6683c792e6720a914509db3d3e6a02204be6b0ad0f67360fbe24dd742fe7830f60168504c676e9c1a99d967319f36c6f"
+                    }
+                ]
+            },
+            {
+                "version": 2,
+                "createdOn": 1475616438,
+                "id": "987773920a42ba771a9cd3d3bb4f109dbe515e380dcb368971cfb1935cb1c16e",
+                "name": "{\"iv\":\"HHd8YNHL6E2stMG926dUeQ==\",\"v\":1,\"iter\":1,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"ct\":\"1rZRlhOk1CFSvbrPCw==\"}",
+                "requestPubKeys": [
+                    {
+                        "key": "020286c2d43a9c8d9956d6a677b13b3767b311ee4c5ebe21834ae2f4f932086c78",
+                        "signature": "3044022011708a30341d0efb41de0559b86c3639eaaa7dfa453464bc182212ef4cece04c02207dc5bd744b362b097a6a65c210395662c41ab0b381659a8a9378e6742555d8d0"
+                    }
+                ]
+            }
+        ],
+        "network": "livenet",
+        "derivationStrategy": "BIP44",
+        "addressType": "P2SH",
+        "scanStatus": "success"
+    },
+    "pendingTxps": [],
+    "preferences": [],
+    "balance": {
+        "totalAmount": 0,
+        "lockedAmount": 0,
+        "totalConfirmedAmount": 0,
+        "lockedConfirmedAmount": 0,
+        "availableAmount": 0,
+        "availableConfirmedAmount": 0,
+        "byAddress": []
+    }
+}
+*/
